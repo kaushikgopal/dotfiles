4a087e505bffb8c3dc34c5bb2b9cff33ac12b5da
kaush <1042037+kaushikgopal@users.noreply.github.com>
2023-07-25 07:32:04 -0700

    update homebrew dependencies

--- .update.sh
+++ .update.sh
@@ -10,15 +10,20 @@ NC='\033[0m' # No Color
 
 
 echo -e "\n\n\n${YELLOW}---- Homebrew updates${NC}"
-echo -e "${PURPLE}---- installing from brewfile${NC}"
-brew bundle install -v --file=".brewfile"
+
 echo -e "${PURPLE}---- clean up to match brewfile ${NC}"
 brew bundle --force cleanup --file=".brewfile"
+
+echo -e "${PURPLE}---- installing from brewfile${NC}"
+brew bundle install -v --file=".brewfile"
+
 echo -e "${PURPLE}---- cask upgrade (via cu) ${NC}"
 brew cu --all --cleanup --yes
+
 echo -e "${PURPLE}\n\n\n\n---- updating formulae${NC}"
 # update the local downloaded git repo with latest code
 brew update
+
 echo -e "${PURPLE}---- upgrading packages${NC}"
 # does the actual upgrade of packages to update formulate from above step
 brew upgrade
