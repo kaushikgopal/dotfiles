ddde979e6ab0c4ad75c6e052cdcba547ac77be44
kaush <1042037+kaushikgopal@users.noreply.github.com>
2024-08-08 21:30:22 -0700

    add alias g. gp

--- .aliases.zsh
+++ .aliases.zsh
@@ -76,18 +76,18 @@ alias t='trash'
 
 
 alias g='git'
+
 alias ga='git a'
 alias gcm='git commit -m'
 alias gd='git d'    # diff with number support
 alias gdc='git dc'  # diff --cached with number support
-
+alias gl='git l'
+alias gll='git ll'
+alias g.='git checkout .'
 alias g-='git checkout -'
 alias gm='git checkout master'
 alias gma='git checkout main'
-
-alias gl='git l'
-alias gll='git ll'
-
+alias gp='git pull'
 alias grc='git rebase --continue'
 alias grv='git remote -v'
 alias gss='git status --short'
