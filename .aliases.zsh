75d38f3366732bebd28cfa308cdc7d804c961d77
kaush <1042037+kaushikgopal@users.noreply.github.com>
2024-09-16 11:50:51 -0700

    cursor

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,85 +1,82 @@
 # better ls
 # alias l='ls -G'        # -G = color
-alias lah='ls -lahG'    # -a = all, -h = human readable, -l = long format
+alias lah='ls -lahG' # -a = all, -h = human readable, -l = long format
 alias ld='tree -d -CFL 1'
 alias tre='tree --dirsfirst -CFL 1'
 
 # use coreutils `ls` if possible…
 hash gls >/dev/null 2>&1 || alias gls="ls"
 # always use color, even when piping (to awk,grep,etc)
-if gls --color > /dev/null 2>&1; then colorflag="--color"; else colorflag="-G"; fi;
+if gls --color >/dev/null 2>&1; then colorflag="--color"; else colorflag="-G"; fi
 export CLICOLOR_FORCE=1
-
-
 
 # verbose these semi-destructive commands
 alias mv='mv -v'
 alias rm='rm -i -v'
 alias cp='cp -v'
 
-
-
 # useful defaults for these specific commands
 alias hi='history -i'
 
 # sharkdp/fd - simple and faster laternative to find
 alias fd='fd -u'
-  # command fd (to override above)
-  # --unrestricted  = include hidden + ignored                # -HI
-  # -g              = provide glob pattern allowing to search with patterns
-  # -p              = by default it will only match the file/directory name
-  #                   this indicates matching full path (so across directories)
-  # -e              = extension type
-  # -t              = file or directory   # -t d is typical
+# command fd (to override above)
+# --unrestricted  = include hidden + ignored                # -HI
+# -g              = provide glob pattern allowing to search with patterns
+# -p              = by default it will only match the file/directory name
+#                   this indicates matching full path (so across directories)
+# -e              = extension type
+# -t              = file or directory   # -t d is typical
 
-  # sample commands
-    # fd -e pdf
-    # fd -e pdf -E taxes          # E is short for --exclude <pattern>
-    #
-    # fd -td '^build$'   # search for directories named build
-    # fd -td build       # substring build (so anywhere in the name)
-    #
-    # fd -te -td     # --type empty --type directory
-    #
-    #
-    # fd -e .gitignore -x trash   # trash each file independently
-    # fd '^\.DS_Store$' -X rm -i  # rm all at once -X implies you execute in the same instance
-    #
-    #
+# sample commands
+# fd -e pdf
+# fd -e pdf -E taxes          # E is short for --exclude <pattern>
+#
+# fd -td '^build$'   # search for directories named build
+# fd -td build       # substring build (so anywhere in the name)
+#
+# fd -te -td     # --type empty --type directory
+#
+#
+# fd -e .gitignore -x trash   # trash each file independently
+# fd '^\.DS_Store$' -X rm -i  # rm all at once -X implies you execute in the same instance
+#
+#
 
 alias rg='rg -uuS'
-    # -uu = --no-ignore --hidden
-    # -S = --smart-case
-    # -I = --no-filename
-    # --no-line-number
-    # --color=never  # when you want to script
-    # -u    = '--no-ignore'.
-    # -uu   = '--no-ignore --hidden'.
-    # -uuu  = '--no-ignore --hidden --binary'.
+# -uu = --no-ignore --hidden
+# -S = --smart-case
+# -I = --no-filename
+# --no-line-number
+# --color=never  # when you want to script
+# -u    = '--no-ignore'.
+# -uu   = '--no-ignore --hidden'.
+# -uuu  = '--no-ignore --hidden --binary'.
 
-  # sample commands
-    # rg fast README.md               # search for literal "fast" in file README.md
-    # rg "com\.android\.application"  # search "pattern"
-    # rg "com\.android\.application" --type gradle  -g '!experimental/*'
+# sample commands
+# rg fast README.md               # search for literal "fast" in file README.md
+# rg "com\.android\.application"  # search "pattern"
+# rg "com\.android\.application" --type gradle  -g '!experimental/*'
 
-    # rg 'fn run' -g '*.rs'           # search all rust files for "fun run"
-    #            --type rust
-    #             -trust
-    #            --type-not rust
-    #
-    # rg fast README.md -r FAST       # replace fast -> FAST
-    #
-    #  lah .git/hooks | rg -v '\.sample'   # invert the match with -v
+# rg 'fn run' -g '*.rs'           # search all rust files for "fun run"
+#            --type rust
+#             -trust
+#            --type-not rust
+#
+# rg fast README.md -r FAST       # replace fast -> FAST
+#
+#  lah .git/hooks | rg -v '\.sample'   # invert the match with -v
 
 alias b='bat'
+alias c='code'
+alias cu='cursor'
 alias i='idea'
-alias ie='idea -e'      # -e light edit mode
+alias ie='idea -e'             # -e light edit mode
 alias iew='idea -e --wait'     # --wait terminal waits for IDE to exit
-alias it='idea --temp-project'  # --temp-project  # create a temporary project
+alias it='idea --temp-project' # --temp-project  # create a temporary project
 alias o='open'
 alias oo='open .'
 alias t='trash'
-
 
 alias g='git'
 
@@ -87,13 +84,13 @@ alias ga='git a'
 alias g.='git add .'
 alias gcm='git commit -m'
 
-alias gd='git d'    # diff with number support
-alias gdc='git dc'  # diff --cached with number support
+alias gd='git d'   # diff with number support
+alias gdc='git dc' # diff --cached with number support
 
 alias gl='git l'
 alias gll='git ll'
 
-alias gch='git ch'  # special checkout git alias
+alias gch='git ch' # special checkout git alias
 alias g-='git checkout -'
 alias gm='git checkout master'
 alias gmp='git checkout master; git pull'
@@ -105,8 +102,6 @@ alias grc='git rebase --continue'
 alias grv='git remote -v'
 alias gss='git status --short'
 
-
-
 # local development
 alias bid='./bin/dev'
 alias bir='./bin/rails'
@@ -116,7 +111,7 @@ alias gw='./gradlew'
 # these were almost .functions.zsh
 alias battery='pmset -g batt'
 #alias battery='pmset -g batt # | grep -Eo "\d+%" | cut -d% -f1'
-alias trashds="find . -name '*.DS_Store' -type f -ls -delete"   # Recursively trash `.DS_Store` files
+alias trashds="find . -name '*.DS_Store' -type f -ls -delete" # Recursively trash `.DS_Store` files
 # Empty the Trash on all mounted volumes and the main HDD. then clear the useless sleepimage
 alias trashempty=" \
     sudo rm -rfv /Volumes/*/.Trashes; \
