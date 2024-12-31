5d67aae540a2ff94a221ed52d4f6eb6fa7689ba9
kaush <1042037+kaushikgopal@users.noreply.github.com>
2024-12-30 17:36:34 -0800

    fish execute new line after every command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# execute a new line after every command
+# this won't be lazy/auto-loaded by fish
+source ~/.config/fish/functions/post-exec.fish
---
+++ .config/fish/functions/post-exec.fish
@@ -0,0 +1,4 @@
+# issue this command after every fish command
+function postexec --on-event fish_postexec
+   echo
+end
