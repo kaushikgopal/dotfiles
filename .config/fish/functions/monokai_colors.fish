2df6efcc714f1f058baaaf100c4c329b7554d7be
kaush <1042037+kaushikgopal@users.noreply.github.com>
2025-01-05 18:21:31 -0800

    monokai.fish colors

---
+++ .config/fish/functions/monokai_colors.fish
@@ -0,0 +1,21 @@
+# Monokai Pro Light Colors
+set fish_color_normal 222222 # default color - dark gray
+set fish_color_command FF6188 # commands - pink/red
+set fish_color_quote D4572A # quoted text - yellow
+set fish_color_redirection AB9DF2 # IO redirections - purple
+set fish_color_end CCCCCC # separators - light gray
+set fish_color_error FFFFFF --background=FF6188 # errors - white on pink/red
+set fish_color_param 218871 # parameters - green
+set fish_color_comment 939293 # comments - medium gray
+set fish_color_match 222222 # matching parentheses - dark gray
+set fish_color_search_match --background=DDDDE1 # search matches - light gray background
+set fish_color_operator AB9DF2 # operators - purple
+set fish_color_escape 78DCE8 # escape characters - light blue
+set fish_color_cwd 78DCE8 # current working directory - light blue
+
+# Completion pager colors
+set fish_pager_color_prefix 222222 # prefix - dark gray
+set fish_pager_color_completion 939293 # completion text - medium gray
+set fish_pager_color_description DDDDE1 # descriptions - light gray
+set fish_pager_color_progress 222222 # progress bar - dark gray
+set fish_pager_color_secondary FFFFFF # secondary background - white
