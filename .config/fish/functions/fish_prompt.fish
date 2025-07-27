5aa32439db170d52f737a627bc93fece955b8d43
kaush <1042037+kaushikgopal@users.noreply.github.com>
2025-07-26 17:57:35 -0700

    [bolt] fish prompt customize

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,36 +1,75 @@
 function fish_prompt
     set -l last_status $status
-
     set -l normal (set_color normal)
     set -l usercolor (set_color $fish_color_user)
 
-    set -l delim (set_color grey) " # " $normal
-    fish_is_root_user; and set delim "#"
+    # ------------------------------------------
+    # [prompt status & character]
 
-    set -l cwd (set_color $fish_color_cwd)
-    if command -sq cksum
-        # randomized cwd color
-        # We hash the physical PWD and turn that into a color. That means directories (usually) get different colors,
-        # but every directory always gets the same color. It's deterministic.
-        # We use cksum because 1. it's fast, 2. it's in POSIX, so it should be available everywhere.
-        set -l shas (pwd -P | cksum | string split -f1 ' ' | math --base=hex | string sub -s 3 | string pad -c 0 -w 6 | string match -ra ..)
-        set -l col 0x$shas[1..3]
+    # Initialize prompt_status as empty.
+    set -l prompt_status
 
-        # If the (simplified idea of) luminance is below 120 (out of 255), add some more.
-        # (this runs at most twice because we add 60)
-        while test (math 0.2126 x $col[1] + 0.7152 x $col[2] + 0.0722 x $col[3]) -lt 120
-            set col[1] (math --base=hex "min(255, $col[1] + 60)")
-            set col[2] (math --base=hex "min(255, $col[2] + 60)")
-            set col[3] (math --base=hex "min(255, $col[3] + 60)")
-        end
-        set -l col (string replace 0x '' $col | string pad -c 0 -w 2 | string join "")
-
-        set cwd (set_color $col)
+    # If the last command failed (status not 0), show the error status in color.
+    if test $last_status -ne 0
+        set prompt_status (set_color $fish_color_status)" $last_status$normal"
     end
 
-    # Prompt status only if it's not 0
-    set -l prompt_status
-    test $last_status -ne 0; and set prompt_status (set_color $fish_color_status)"[$last_status]$normal"
+    # Set the default prompt delimiter with color.
+    set -l delim (set_color brblack)" #" $normal
+
+    # # If running as root, use a plain "#" as the delimiter (no color).
+    # if fish_is_root_user
+    #     set delim "#"
+    # end
+
+    # ------------------------------------------
+    # [directory]
+
+    # cwd = current working directory color
+    # set -l cwd (set_color $fish_color_cwd)
+    # if command -sq cksum
+    #     # randomized cwd color
+    #     # We hash the physical PWD and turn that into a color. That means directories (usually) get different colors,
+    #     # but every directory always gets the same color. It's deterministic.
+    #     # We use cksum because 1. it's fast, 2. it's in POSIX, so it should be available everywhere.
+    #     set -l shas (pwd -P | cksum | string split -f1 ' ' | math --base=hex | string sub -s 3 | string pad -c 0 -w 6 | string match -ra ..)
+    #     set -l col 0x$shas[1..3]
+
+    #     # If the (simplified idea of) luminance is below 120 (out of 255), add some more.
+    #     # (this runs at most twice because we add 60)
+    #     while test (math 0.2126 x $col[1] + 0.7152 x $col[2] + 0.0722 x $col[3]) -lt 120
+    #         set col[1] (math --base=hex "min(255, $col[1] + 60)")
+    #         set col[2] (math --base=hex "min(255, $col[2] + 60)")
+    #         set col[3] (math --base=hex "min(255, $col[3] + 60)")
+    #     end
+    #     set -l col (string replace 0x '' $col | string pad -c 0 -w 2 | string join "")
+    #     set cwd (set_color -b brblack $col)
+    # end
+    set -l pwd (set_color -b brblack black)" "(prompt_pwd)" "(set_color normal)
+
+    # ------------------------------------------
+    # [git branch]
+
+    # set -g __fish_git_prompt_showdirtystate 1
+    # set -g __fish_git_prompt_showuntrackedfiles 1
+    # set -g __fish_git_prompt_showupstream informative
+    # set -g __fish_git_prompt_showcolorhints 1
+    # set -g __fish_git_prompt_use_informative_chars 1
+    # # Unfortunately this only works if we have a sensible locale
+    # string match -qi "*.utf-8" -- $LANG $LC_CTYPE $LC_ALL
+    # and set -g __fish_git_prompt_char_dirtystate "*"
+    # set -g __fish_git_prompt_char_untrackedfiles "?"
+
+    # # The git prompt's default format is ' (%s)'.
+    # # We don't want the leading space.
+    # set -l vcs (fish_vcs_prompt '%s' 2>/dev/null)
+    set -l vcs (fish_vcs_prompt)
+
+
+
+
+    # ------------------------------------------
+    # [prompt_host]
 
     # Only show host if in SSH or container
     # Store this in a global variable because it's slow and unchanging
@@ -45,8 +84,8 @@ function fish_prompt
         end
     end
 
-    # Shorten pwd if prompt is too long
-    set -l pwd (prompt_pwd)
+    # ------------------------------------------
+    # assemble prompt
 
-    echo -n -s $prompt_host $cwd $pwd $normal $prompt_status $delim
+    string join "" -- $prompt_host $pwd $vcs $normal $prompt_status $delim
 end
