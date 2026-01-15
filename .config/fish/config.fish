ca05782b4f25d49d70ec085581c8aea975e950c3
KG <1042037+kaushikgopal@users.noreply.github.com>
2026-01-14 18:59:29 -0800

    gemini default yolo abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -69,7 +69,7 @@ if status is-interactive
 
     abbr --add --global co  codex --yolo
     abbr --add --global cl  claude --dangerously-skip-permissions
-    abbr --add --global ge  gemini
+    abbr --add --global ge  gemini --yolo
     # abbr --add --global cu  cursor
     # abbr --add --global cug cursor . -g
     abbr --add --global z   zed
