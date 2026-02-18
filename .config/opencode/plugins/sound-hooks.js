7facd945e4807fb9d2a2f0c00b8b3ede3126e69b
KG <1042037+kaushikgopal@users.noreply.github.com>
2026-02-17 18:57:32 -0800

    xdg data for sound hooks

--- .config/opencode/plugins/sound-hooks.js
+++ .config/opencode/plugins/sound-hooks.js
@@ -2,11 +2,11 @@ export const SoundHooks = async ({ $ }) => {
   return {
     event: async ({ event }) => {
       if (event.type === "tui.toast.show") {
-        await $`afplay $XDG_STATE_HOME/sound-notification-human`.quiet();
+        await $`afplay $XDG_DATA_HOME/sound-notification-orc`.quiet();
       }
 
       if (event.type === "session.idle") {
-        await $`afplay $XDG_STATE_HOME/sound-done-human`.quiet();
+        await $`afplay $XDG_DATA_HOME/sound-done-orc`.quiet();
       }
     },
   };
